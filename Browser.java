50e8ca7d2fbc903fac886a33c7eb2bf2d90369dd
Sumitha0207 <159099940+Sumitha0207@users.noreply.github.com>
2024-02-09 19:52:40 +0530

    Add files via upload
    week1day1

---
+++ Browser.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) 
+	{
+		float browserVersion=12.2f;
+		String browserName="FireFox";
+		boolean pageLoaded=true;
+		int noOfbrowser=1;
+		char browserLogo='F';
+		System.out.println("Browser Version :"+browserVersion);
+		System.out.println("Browser Name :"+browserName);
+		System.out.println("Browser Opened :"+pageLoaded);
+		System.out.println("Number Of Browsers :"+noOfbrowser);
+		System.out.println("Browser Logo :"+browserLogo);
+	}
+
+}
---
+++ FindNumber.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) 
+	{
+		int a=-4;
+		if (a>0)
+		{
+			System.out.println("The number is positive number :"+a);
+		
+		}
+		else 
+		{
+			System.out.println("The number is negative number :"+a);
+		}
+
+	}
+
+}
